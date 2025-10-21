bb076945d1f6bb309c25da504170aed651133c78
Jan-Georges Jersild Balin <jgbalin@gmail.com>
2025-10-21 11:33:07 +0200

    clean

--- led/led_test.py
+++ led/led_test.py
@@ -1,16 +1,11 @@
-
-
-import time
-
-from led import LED
-from led import TrafficLight
+from traffic_light import TrafficLight
 GREEN_PIN = 17
 YELLOW_PIN = 27
 RED_PIN = 22
 
 try:
     print("Initializing test")
-    traffic_light = TrafficLight(LED(GREEN_PIN), LED(YELLOW_PIN), LED(RED_PIN))
+    traffic_light = TrafficLight(GREEN_PIN, YELLOW_PIN, RED_PIN)
     print("traffic light initialized")
     while True:
         traffic_light.dance()
