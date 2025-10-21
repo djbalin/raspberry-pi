953327751d37cb739bbd5ec25d273766c9aa67c0
Jan (Pi) <jgbalin@gmail.com>
2025-10-21 11:26:31 +0200

    clean

--- led/led.py
+++ led/led.py
@@ -4,12 +4,6 @@ import time
 # Use BCM pin numbering
 GPIO.setmode(GPIO.BCM)
 
-# Define GPIO pins for LEDs (match your wiring)
-GREEN_PIN = 17  # row 35 LED anode wire
-YELLOW_PIN = 27  # row 40 LED anode wire
-RED_PIN = 22  # row 45 LED anode wire
-
-
 class LED:
     def __init__(self, pin):
         self.pin = pin
@@ -17,9 +11,11 @@ class LED:
 
     def on(self):
         GPIO.output(self.pin, GPIO.HIGH)
+        print(self.pin)
 
     def off(self):
         GPIO.output(self.pin, GPIO.LOW)
+        print(self.pin)
 
     def toggle(self):
         GPIO.output(self.pin, GPIO.HIGH if GPIO.input(self.pin) == GPIO.LOW else GPIO.LOW)
